01a6689b78e2610a7ee8ea78a3695fd5fe6af847
StevenSYS <139715581+StevenSYS@users.noreply.github.com>
2025-07-25 12:28:31 +0000

    The random index variable is now directly used instead of being increased using a function
    Also added a modified version of the random generator C file

---
+++ src/generators/randomGenerator.c
@@ -0,0 +1,39 @@
+#!/bin/tcc -run
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+	srand(510);
+	
+	FILE *file = fopen("../random.HC", "w");
+	
+	if (file == NULL) {
+		perror("ERROR: Failed to open/make file");
+		return 1;
+	}
+	
+	fprintf(file, "/* [AUTO GENERATED] */\n");
+	fprintf(file, "U8 table[256] = {\n");
+	for (unsigned char y = 0; y < 16; y++) {
+		fprintf(file, "	");
+		for (unsigned char x = 0; x < 16; x++) {
+			fprintf(file, "%u", (rand() % 255) % 12);
+			if ((x != 15) || (y != 15)) {
+				fprintf(file, ", ");
+			}
+		}
+		fprintf(file, "\n");
+	}
+	fprintf(file, "};\n");
+	fprintf(file, "U8 random_index = 0;\n\n"),
+	fprintf(file, "U8 random_get() {\n");
+	fprintf(file, "\t/*\n");
+	fprintf(file, "\t\tSince this is a U8,\n");
+	fprintf(file, "\t\tit will loop back to 0 when it goes over 255\n");
+	fprintf(file, "\t*/\n");
+	fprintf(file, "\trandom_index++;\n");
+	fprintf(file, "\treturn table[random_index];\n");
+	fprintf(file, "}");
+	return 0;
+}
