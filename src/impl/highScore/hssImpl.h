7afeb5ab99711b7238429670794e3ce792c03e90
StevenSYS <139715581+StevenSYS@users.noreply.github.com>
2025-08-03 01:44:26 +0000

    Changed "5HS" to "1HS"

--- src/impl/highScore/hssImpl.h
+++ src/impl/highScore/hssImpl.h
@@ -2,11 +2,11 @@
 #define __HSSIMPL__
 
 #ifdef FILENAME83
-	#define HSSIMPL_FILENAME "HGHSCORE.5HS"
+	#define HSSIMPL_FILENAME "HGHSCORE.1HS"
 #else
-	#define HSSIMPL_FILENAME "highScore.5hs"
+	#define HSSIMPL_FILENAME "highScore.1hs"
 #endif
-#define HSSIMPL_SIGNATURE "5HS\x57\xE6\xE4\x56\x50"
+#define HSSIMPL_SIGNATURE "1HS\x57\xE6\xE4\x56\x50"
 #define HSSIMPL_LENGTH_BUFFER 20
 
 unsigned int hssImpl_get();
